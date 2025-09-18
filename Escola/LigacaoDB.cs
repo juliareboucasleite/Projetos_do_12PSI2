034ae9c29f53740e82dfbf42c8c7dd0600f66b37
Julia Rebouças <69313019+juliareboucasleite@users.noreply.github.com>
2025-09-18 14:16:25 +0100

    Adicionar classe para ligação à base de dados

---
+++ Escola/LigacaoDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+    internal class LigacaoDB
+    {
+        /// <summary>
+        /// Endereço do servidor.
+        /// </summary>
+        public static string Servidor { get; private set; } = "127.0.0.1";
+        /// <summary>
+        /// Nome do utilizador.
+        /// </summary>
+        public static string Utilizador { get; private set; } = "root";
+        /// <summary>
+        /// Password do utilizador.
+        /// </summary>
+        public static string Password { get; private set; } = string.Empty;
+        /// <summary>
+        /// Base de dados a utilizar.
+        /// </summary>
+        public static string DB { get; private set; } = "escola";
+        /// <summary>
+        /// Obter a string de ligação (connection string) ao servidor de base de dados.
+        /// </summary>
+        public static string GetConnectionString()
+        {
+            return
+            $"Server={Servidor};database={DB};Uid={Utilizador};Pwd={Password};Charset=utf8mb4;Port = 3306; SslMode = none";
+        }
+    }
+}
