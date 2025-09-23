e26ebd32833c2bfcfd7c68d7b12a3b6adb2db8d9
Julia Rebouças <69313019+juliareboucasleite@users.noreply.github.com>
2025-09-23 14:11:01 +0100

    Adicionar a classe Aluno

---
+++ Escola/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+    /// <summary>
+    /// Representa um aluno.
+    /// </summary>
+    internal class Aluno
+    {
+        public int ID { get; set; }
+        public int NumeroProcesso { get; set; }
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public string Morada { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
