48ae69be41b48ec5810c4a0ddc7a46959b7c248f
Julia Rebouças <69313019+juliareboucasleite@users.noreply.github.com>
2025-09-25 14:16:25 +0100

    Inicializar FormAutenticacao

--- Escola/FormAutenticacao.Designer.cs
+++ Escola/FormAutenticacao.Designer.cs
@@ -28,29 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Utilizador = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.LabelFeedback = new System.Windows.Forms.Label();
+            this.BotaoOk = new System.Windows.Forms.Button();
+            this.BotaoCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Utilizador
             // 
-            this.textBox1.Location = new System.Drawing.Point(122, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 22);
-            this.textBox1.TabIndex = 2;
+            this.Utilizador.Location = new System.Drawing.Point(122, 74);
+            this.Utilizador.Name = "Utilizador";
+            this.Utilizador.Size = new System.Drawing.Size(195, 22);
+            this.Utilizador.TabIndex = 2;
             // 
-            // textBox2
+            // Password
             // 
-            this.textBox2.Location = new System.Drawing.Point(122, 115);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(195, 22);
-            this.textBox2.TabIndex = 4;
+            this.Password.Location = new System.Drawing.Point(122, 115);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(195, 22);
+            this.Password.TabIndex = 4;
             // 
             // label1
             // 
@@ -80,49 +80,50 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "&Password:";
             // 
-            // label4
+            // LabelFeedback
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(125, 147);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.LabelFeedback.AutoSize = true;
+            this.LabelFeedback.Location = new System.Drawing.Point(125, 147);
+            this.LabelFeedback.Name = "LabelFeedback";
+            this.LabelFeedback.Size = new System.Drawing.Size(38, 13);
+            this.LabelFeedback.TabIndex = 5;
+            this.LabelFeedback.Text = "label4";
             // 
-            // button1
+            // BotaoOk
             // 
-            this.button1.Location = new System.Drawing.Point(223, 182);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "&OK";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BotaoOk.Location = new System.Drawing.Point(223, 182);
+            this.BotaoOk.Name = "BotaoOk";
+            this.BotaoOk.Size = new System.Drawing.Size(75, 23);
+            this.BotaoOk.TabIndex = 6;
+            this.BotaoOk.Text = "&OK";
+            this.BotaoOk.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // BotaoCancelar
             // 
-            this.button2.Location = new System.Drawing.Point(315, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "&Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BotaoCancelar.Location = new System.Drawing.Point(315, 181);
+            this.BotaoCancelar.Name = "BotaoCancelar";
+            this.BotaoCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BotaoCancelar.TabIndex = 7;
+            this.BotaoCancelar.Text = "&Cancelar";
+            this.BotaoCancelar.UseVisualStyleBackColor = true;
             // 
             // FormAutenticacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(417, 228);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BotaoCancelar);
+            this.Controls.Add(this.BotaoOk);
+            this.Controls.Add(this.LabelFeedback);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Utilizador);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FormAutenticacao";
             this.Text = "FormAutenticacao";
+            this.Load += new System.EventHandler(this.FormAutenticacao_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,13 +131,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Utilizador;
+        private System.Windows.Forms.TextBox Password;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label LabelFeedback;
+        private System.Windows.Forms.Button BotaoOk;
+        private System.Windows.Forms.Button BotaoCancelar;
     }
 }
--- Escola/FormAutenticacao.cs
+++ Escola/FormAutenticacao.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Dapper;
 
 namespace Escola
 {
@@ -15,6 +17,22 @@ namespace Escola
         public FormAutenticacao()
         {
             InitializeComponent();
+            // A janela deverá surgir no centro do ecrã
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void FormAutenticacao_Load(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+            this.AcceptButton = BotaoOk;
+            this.CancelButton = BotaoCancelar;
+            this.Text = "Autenticação";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            LabelFeedback.Visible = false;
+            Password.UseSystemPasswordChar = true;
+            this.ResumeLayout(false);
         }
     }
 }
